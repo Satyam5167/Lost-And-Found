81418e810bb53a760e47d6790910984bc8a0e128
Satyam5167 <satyamgdbg@gmail.com>
2025-10-22 14:46:48 +0530

    forcig ipv4

--- server/db.js
+++ server/db.js
@@ -1,6 +1,8 @@
 // server/db.js
 import pg from 'pg';
 const Pool = pg.Pool;
+// Force Node to prefer IPv4 instead of IPv6
+dns.setDefaultResultOrder("ipv4first");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
